550c6da70e8d9fede91ff2186298daf06700a73a
James M Wilson <jmw@fastmail.com>
2019-03-03 09:45:32 -0800

    Add remaining packages

--- stage6/00-sys-tweaks/01-run.sh
+++ stage6/00-sys-tweaks/01-run.sh
@@ -6,7 +6,7 @@ install -m 644 files/99-usbtmc.rules "${ROOTFS_DIR}/etc/udev/rules.d/"
 
 install -d "${ROOTFS_DIR}/var/cache/apt/archives"
 install -m 644 files/seeed-voicecard_0.3+git20190227-1_all.deb "${ROOTFS_DIR}/var/cache/apt/archives"
-install -m 644 files/seeed-voicecard-dkms_0.3+git20190227-1_armhf.deb "${ROOTFS_DIR}/var/cache/apt/archives"
+install -m 644 files/seeed-voicecard-dkms_0.3+git20190227-1_all.deb "${ROOTFS_DIR}/var/cache/apt/archives"
 
 on_chroot apt-key add - < files/snips.gpg.key
 on_chroot << EOF
@@ -14,5 +14,5 @@ apt-get update
 groupadd -f usbtmc
 adduser ${FIRST_USER_NAME} usbtmc
 dpkg --install /var/cache/apt/archives/seeed-voicecard_0.3+git20190227-1_all.deb
-dpkg --install /var/cache/apt/archives/seeed-voicecard-dkms_0.3+git20190227-1_armhf.deb
+dpkg --install /var/cache/apt/archives/seeed-voicecard-dkms_0.3+git20190227-1_all.deb
 EOF
--- stage6/01-install-snips/01-run.sh
+++ stage6/01-install-snips/01-run.sh
@@ -1,7 +1,14 @@
 #!/bin/bash -e
 
-install -m 644 files/assistant_proj_QPypyO6X1M.zip "${ROOTFS_DIR}/usr/share/snips"
+unzip files/assistant_proj_QPypyO6X1M.zip -d "${ROOTFS_DIR}/usr/share/snips"
+install -m 644 files/ollie_1.0_all.deb "${ROOTFS_DIR}/var/cache/apt/archives"
+install -m 644 files/python3-paho-mqtt_1.4.0-1_all.deb "${ROOTFS_DIR}/var/cache/apt/archives"
+install -m 644 files/python3-usbtmc_0.8-1_all.deb "${ROOTFS_DIR}/var/cache/apt/archives"
+install -m 644 files/python3-usb_1.0.2-1_all.deb "${ROOTFS_DIR}/var/cache/apt/archives"
+
 on_chroot << EOF
-unzip /usr/share/snips/assistant_proj_QPypyO6X1M.zip -d /usr/share/snips
-rm -f /usr/share/snips/assistant_proj_QPypyO6X1M.zip
+dpkg --install /var/cache/apt/archives/ollie_1.0_all.deb \
+  /var/cache/apt/archives/python3-paho-mqtt_1.4.0-1_all.deb \
+  /var/cache/apt/archives/python3-usbtmc_0.8-1_all.deb \
+  /var/cache/apt/archives/python3-usb_1.0.2-1_all.deb
 EOF
