fabd83023ddce892398d87f71f0e7c2266dfde7e
James M Wilson <jmw@fastmail.com>
2019-03-09 21:08:02 -0800

    Switch from packages in repo to PPA; update packages

---
+++ stage6/00-sys-tweaks/00-run.sh
@@ -0,0 +1,11 @@
+#!/bin/bash -e
+
+install -m 644 files/ppa.list "${ROOTFS_DIR}/etc/apt/sources.list.d/"
+install -m 644 files/snips.list "${ROOTFS_DIR}/etc/apt/sources.list.d/"
+install -m 644 files/50-var.conf "${ROOTFS_DIR}/etc/tmpfiles.d/"
+
+on_chroot apt-key add - < files/ppa.gpg.key
+on_chroot apt-key add - < files/snips.gpg.key
+on_chroot << EOF
+apt-get update
+EOF
