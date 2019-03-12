286ac16a9cd3d17b5236552b53a91979d6eb8b1d
James M Wilson <jmw@fastmail.com>
2019-03-11 22:35:22 -0700

    Remove network wait on startup

--- stage6/00-sys-tweaks/00-run.sh
+++ stage6/00-sys-tweaks/00-run.sh
@@ -1,5 +1,7 @@
 #!/bin/bash -e
 
+rm -f "${ROOTFS_DIR}/etc/systemd/system/dhcpcd.service.d/wait.conf"
+
 install -m 644 files/ppa.list "${ROOTFS_DIR}/etc/apt/sources.list.d/"
 install -m 644 files/snips.list "${ROOTFS_DIR}/etc/apt/sources.list.d/"
 install -m 644 files/50-var.conf "${ROOTFS_DIR}/etc/tmpfiles.d/"
